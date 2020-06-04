e334263b9fd5d26ca10c0b55e5ee27ec584cee90
Ronald Konjer <ronaldkonjer@gmail.com>
2020-06-04 21:43:40 +0200

    add defaults

--- variables.tf
+++ variables.tf
@@ -135,11 +135,13 @@ variable "vpc_id" {
 variable "namespace" {
   description = "Namespace (e.g. `eg` or `cp`)"
   type        = string
+  default     = ""
 }
 
 variable "stage" {
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
   type        = string
+  default     = ""
 }
 
 variable "delimiter" {
