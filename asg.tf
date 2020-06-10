300f0afc45f7b7b2d3d8e3f3ca13864494a3f24f
Ronald Konjer <ronaldkonjer@gmail.com>
2020-06-11 01:33:55 +0200

    bug fix

--- asg.tf
+++ asg.tf
@@ -35,9 +35,11 @@ resource "aws_launch_configuration" "zookeeper" {
   image_id                    = data.aws_ami.zookeeper.id
   instance_type               = var.instance_type
   key_name                    = var.keyname
-  name                        = module.label.id
   security_groups             = compact(concat([
     aws_security_group.zookeeper.id,
     aws_security_group.zookeeper_intra.id], var.extra_security_group_ids))
   user_data                   = data.template_file.zookeeper_asg[0].rendered
+  lifecycle {
+    create_before_destroy = true
+  }
 }
